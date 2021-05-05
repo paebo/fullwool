edfa439790e31aab2fa48d8c53356e7a67efadf0
JenesesD <jenesesd@gmail.com>
2021-05-05 15:11:45 +0200

    created sql file for db

---
+++ backend/sql/db_init.sql
@@ -0,0 +1,49 @@
+-- DROP DATABASE IF EXISTS fullwool;
+-- CREATE DATABASE fullwool;
+
+DROP TABLE IF EXISTS account;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS about;
+DROP TABLE IF EXISTS gallery;
+DROP TABLE IF EXISTS shop;
+
+
+CREATE TABLE account
+(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(20),
+    pw VARCHAR(255)
+);
+
+CREATE TABLE post
+(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50),
+    msg TEXT,
+    account_id INT,
+    CONSTRAINT fk_account_id FOREIGN KEY (account_id) REFERENCES account(id)
+);
+
+CREATE TABLE about
+(
+    id SERIAL PRIMARY KEY,
+    bio TEXT,
+    img VARCHAR
+);
+
+CREATE TABLE gallery
+(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50),
+    category VARCHAR(30),
+    img VARCHAR
+);
+
+CREATE TABLE shop
+(
+    id SERIAL PRIMARY KEY,
+    item_name VARCHAR(50),
+    price INT,
+    details TEXT,
+    img VARCHAR
+)
