0c4c31cf2a460d7711a36987e99fe25dbd9c7bf6
Paebo <petibors2@gmail.com>
2021-05-10 14:07:49 +0200

    add real routes to header, create necessarry components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,31 +2,48 @@ import "./App.css";
 import { useState, useEffect, useContext } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import axios from "axios";
+import About from "./pages/About";
+import Gallery from "./pages/Gallery";
+import CourseList from "./pages/CourseList";
+import Shop from "./pages/Shop";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
 
 function App() {
-  const [items, setItems] = useState([]);
+    const [items, setItems] = useState([]);
 
-  const fetchItems = async () => {
-    const response = await axios.get("http://localhost:8081/about");
-    setItems(response);
-  };
+    const fetchItems = async () => {
+        const response = await axios.get("http://localhost:8081/about");
+        setItems(response);
+    };
 
-  useEffect(() => {
-    fetchItems();
-    return () => {};
-  }, []);
+    useEffect(() => {
+        fetchItems();
+        return () => {};
+    }, []);
 
-  console.log(items);
+    console.log(items);
 
-  return (
-    <Router>
-      <div className='App'>
-        <header className='App-header'>
-          Rólam | Galéria | Tanfolyamok | Áruház | Blog
-        </header>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className='App'>
+                <header className='App-header'>
+                    <Link to='/about'>Rólam</Link>
+                    <Link to='/gallery'>Galéria</Link>
+                    <Link to='/courses'>Tanfolyamok</Link>
+                    <Link to='/shop'>Áruház</Link>
+                    <Link to='/blog'>Blog</Link>
+                    <Link to='/contact'>Kapcsolat</Link>
+                </header>
+                <Route path='/about' component={About} />
+                <Route path='/gallery' component={Gallery} />
+                <Route path='/courses' component={CourseList} />
+                <Route path='/shop' component={Shop} />
+                <Route path='/blog' component={Blog} />
+                <Route path='/contact' component={Contact} />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ frontend/src/pages/CourseList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CourseList() {
+    return (
+        <div>
+            <h1>Courses</h1>
+        </div>
+    );
+}
