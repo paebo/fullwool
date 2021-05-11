af19b52fc879a26c0a1f61a1baa2ac7d18bd8fdb
JenesesD <jenesesd@gmail.com>
2021-05-11 08:56:27 +0200

    moved queries.js into model folder

--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../../queries");
+const db = require("../../model/queries");
 const cors = require("cors");
 
 const corsOptions = {
