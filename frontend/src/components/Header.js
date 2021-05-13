3b27bd5d919cb1f22f9268b8bebe922d2f06f2fa
Paebo <petibors2@gmail.com>
2021-05-13 12:02:42 +0200

    change to yarn from npm

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Navbar = styled.nav`
+    padding: 2%;
+    background: papayawhip;
+`;
 
 function Header() {
     return (
-        <div className='header'>
+        <Navbar className='header'>
             <Link to='/about'>Rólam | </Link>
             <Link to='/gallery'>Galéria | </Link>
             <Link to='/courses'>Tanfolyamok | </Link>
             <Link to='/shop'>Áruház | </Link>
             <Link to='/blog'>Blog | </Link>
             <Link to='/contact'>Kapcsolat</Link>
-        </div>
+        </Navbar>
     );
 }
 
