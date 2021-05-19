0e7263bedd5547dfb16b18447b7eae2c1d59bf35
JenesesD <jenesesd@gmail.com>
2021-05-19 16:17:04 +0200

    copied tests to match the /contact endpoint

--- backend/test/index.test.js
+++ backend/test/index.test.js
@@ -3,6 +3,7 @@ const supertest = require("supertest");
 const request = supertest(app);
 const pg = require("pg");
 
+// TODO: follow up on Jest not exiting properly
 
 describe("GET /about", () => {
     it("sends a request to endpoint", async done => {
@@ -38,6 +39,26 @@ describe("GET /gallery", () => {
     
     it("gets json object response", async done => {
         const res = await request.get("/about");
+        expect(res.type).toBe("application/json");
+        done();
+    });    
+})
+
+
+describe("GET /contact", () => {
+    it("sends a request to endpoint", async done => {
+        const res = await request.get("/contact");
+        done();
+    });
+    
+    it("gets status code 200 from endpoint", async done => {
+        const res = await request.get("/contact");
+        expect(res.status).toBe(200);
+        done();
+    });
+    
+    it("gets json object response", async done => {
+        const res = await request.get("/contact");
         expect(res.type).toBe("application/json");
         done();
     });    
