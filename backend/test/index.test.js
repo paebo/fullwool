94221a552ee211a23f2f6c503b5cb7ae1aa710c0
JenesesD <jenesesd@gmail.com>
2021-05-19 16:15:08 +0200

    copied tests to match the /gallery endpoint

--- backend/test/index.test.js
+++ backend/test/index.test.js
@@ -5,20 +5,40 @@ const pg = require("pg");
 
 
 describe("GET /about", () => {
-    it("sends a request to /about endpoint", async done => {
+    it("sends a request to endpoint", async done => {
         const res = await request.get("/about");
         done();
     });
     
-    it("gets status code 200 from /about endpoint", async done => {
+    it("gets status code 200 from endpoint", async done => {
         const res = await request.get("/about");
         expect(res.status).toBe(200);
         done();
     });
     
-    it("gets a response as json from /about", async done => {
+    it("gets json object response", async done => {
         const res = await request.get("/about");
         expect(res.type).toBe("application/json");
         done();
     });
 });
+
+
+describe("GET /gallery", () => {
+    it("sends a request to endpoint", async done => {
+        const res = await request.get("/gallery");
+        done();
+    });
+    
+    it("gets status code 200 from endpoint", async done => {
+        const res = await request.get("/gallery");
+        expect(res.status).toBe(200);
+        done();
+    });
+    
+    it("gets json object response", async done => {
+        const res = await request.get("/about");
+        expect(res.type).toBe("application/json");
+        done();
+    });    
+})
